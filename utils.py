bf51ee8e81feec10133c0498848a2435e3673a35
Mli217 <3022907008@qq.com>
2026-06-09 15:36:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # utils.py
 import math
 
+# 常量定义
 x_pi = 3.14159265358979324 * 3000.0 / 180.0
 pi = 3.1415926535897932384626
 a = 6378245.0
@@ -21,6 +22,14 @@ def _transform_lng(lng, lat):
     return ret
 
 def wgs84_to_gcj02(lng, lat):
+    """
+    WGS-84 坐标系转 GCJ-02
+    :param lng: 经度
+    :param lat: 纬度
+    :return: (gcj_lng, gcj_lat)
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
     dlat = _transform_lat(lng - 105.0, lat - 35.0)
     dlng = _transform_lng(lng - 105.0, lat - 35.0)
     radlat = lat / 180.0 * pi
@@ -34,10 +43,19 @@ def wgs84_to_gcj02(lng, lat):
     return mglng, mglat
 
 def gcj02_to_wgs84(lng, lat):
+    """
+    GCJ-02 坐标系转 WGS-84
+    :param lng: 经度
+    :param lat: 纬度
+    :return: (wgs_lng, wgs_lat)
+    """
     if out_of_china(lng, lat):
         return lng, lat
     dlng, dlat = wgs84_to_gcj02(lng, lat)
     return lng * 2 - dlng, lat * 2 - dlat
 
 def out_of_china(lng, lat):
+    """
+    判断是否在中国境外，不在国内则不进行偏移转换
+    """
     return not (72.004 <= lng <= 137.8347 and 0.8293 <= lat <= 55.8271)
